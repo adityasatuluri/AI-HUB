151bc256a30f3387fa36dfb260a85a9f01a8af42
Aditya Satuluri <s.aditya.in@gmail.com>
2024-09-27 20:08:03 +0530

    developer update

--- layout.py
+++ layout.py
@@ -18,7 +18,7 @@ def sidebar_layout():
             <div>
                 <hr>
                 <p style='margin: 0;'>Open for <a style="color:red" href='https://github.com/adityasatuluri/AI-HUB' target='_blank'>Contributions</a></p>
-                <p style='margin: 0; color:#919191'>Developed by <a style="color:#b82f23" href='https://github.com/adityasatuluri' target='_blank'>Aditya Satuluri</a></p>
+                <p style='margin: 0; color:#919191'>Developed by <a style="color:#b82f23" href='https://github.com/adityasatuluri' target='_blank'>Aditya Satuluri</a> and <a style="color:#b82f23" href='https://github.com/reguvel' target='_blank'>Reguvel Gnanavelu</a></p>
             </div>
             """,
             unsafe_allow_html=True
